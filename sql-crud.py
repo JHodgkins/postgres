89f9a228a2723fdf7b31beabbf15be392ba5b115
JHodgkins <jhodgkins78@gmail.com>
2022-06-23 04:12:17 +0000

    initilize programmer class for adding data to new table

---
+++ sql-crud.py
@@ -0,0 +1,30 @@
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# exercuting the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+# create a class based model for the "programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaaker, then point to our engine (the db)
+Session = sessionmaker(db)
+
+# open an actual session by calling the session() subclass defined above
+session = Session()
+
+# creating the database using declarative_base subclass
+base.metadata.create_all(db)
