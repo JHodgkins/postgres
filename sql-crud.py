880a9e605027f63b73e6833316b41ace03f783ae
JHodgkins <jhodgkins78@gmail.com>
2022-06-23 06:41:20 +0000

    create and read function added

--- sql-crud.py
+++ sql-crud.py
@@ -28,3 +28,93 @@ session = Session()
 
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
+
+# Creating records on our "Programmers" table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First programmer"
+)
+
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL langusge"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_burners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
+steve_jobs = Programmer(
+    first_name="Steve",
+    last_name="Jobs",
+    gender="M",
+    nationality="American",
+    famous_for="Apple"
+)
+
+james_hodgkins = Programmer(
+    first_name="James",
+    last_name="Hodgkins",
+    gender="M",
+    nationality="British",
+    famous_for="Blind programmer"
+)
+
+# Add each instane of our programmers to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_burners_lee)
+session.add(steve_jobs)
+session.add(james_hodgkins)
+
+# commit our session to the database
+session.commit()
+
+# Query the database to find all programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
